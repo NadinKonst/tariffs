51b4d513c7d3407d327611749190fdca43a90429
NadinKonst <nadikon@gmail.com>
2024-07-29 21:38:23 +0700

    add text button

---
+++ src/components/Text/Text.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import './Text.scss';
+
+export default function AddText() {
+  const [newText, setNewText] = useState("");
+  const [showText, setShowText] = useState("");
+
+  const handleAddText = (e) => {
+    setNewText(e.target.value);
+  };
+
+  const handleShowText = () => {
+    setShowText(newText);
+    setNewText("");
+  };
+
+  return (
+    <div className="container">
+      <input type="text" required value={newText} onChange={handleAddText} />
+      <button type="button" onClick={handleShowText}>
+        Add Text
+      </button>
+      <p>{showText}</p>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Card from "./components/Card/Card.jsx";
 import "./index.scss";
+import Text from "./components/Text/Text.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Text />
     <Card />
   </React.StrictMode>
 );
